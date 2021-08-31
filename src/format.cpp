e443abfc116c1e12dc5325baf754ee7c2a0ed5a5
stephanstu <stephan.studener@googlemail.com>
2021-08-31 13:56:39 +0000

    Updtaed format of time output.

--- src/format.cpp
+++ src/format.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <sstream>
+#include <iomanip>
 
 #include "format.h"
 
@@ -8,13 +9,19 @@ using std::string;
 // TODO: Complete this helper function -> OK
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
+// Taken from 
+// - https://knowledge.udacity.com/questions/155686 
+// - https://docs.microsoft.com/de-de/cpp/standard-library/iomanip-functions?view=msvc-160#setfill, 
+// - https://stackoverflow.com/questions/48310746/converting-time-into-hours-minutes-and-seconds
+// iomanip-headers make it possible to format outputs as required here.
 string Format::ElapsedTime(long seconds) { 
   int hours, mins, secs;
-  std::stringstream elapsedTime;
+  std::ostringstream elapsedTime;
   hours = (seconds / 3600) % 100;
   mins = (seconds / 60) % 60;
   secs = seconds % 60;
-  elapsedTime << hours << ":" << mins << ":" << secs;
+  elapsedTime  << std::setw(2) << std::setfill('0') << hours << ":" 
+     << std::setw(2) << std::setfill('0') << mins << ":"
+     << std::setw(2) << std::setfill('0') << secs;
   return elapsedTime.str();
 }
